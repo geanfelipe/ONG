acf8e448ffd735c4c9fbc5ff2f8a0398f84b0fe8
Gean Felipe <geanfelipemoura@outlook.com>
2015-02-12 15:06:26 -0300

    databases

--- projeto/settings.py
+++ projeto/settings.py
@@ -59,16 +59,23 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
+"""
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django_postgrespool'
+        }
+    }
+"""
 #HEROKU
+DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 DATABASES['default']['ENGINE']='django_postgrespool'
 
