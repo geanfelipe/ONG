20c22396693da981d8cb54b51f3147eeb1bc1788
Gean Felipe <geanfelipemoura@outlook.com>
2015-02-13 01:31:43 -0300

    views

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 
 def index(request):
 
-	return HttpResponse("<br> <h1>WELCOME TO BLABLA ONG </h1></br>")
+	return HttpResponse("<br> <h1>WELCOME TO Vai Q Cola ONG </h1></br>")
 
