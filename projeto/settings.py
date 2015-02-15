da574fb4275d6519337d5f66c7cc90cb6442a8d6
Gean Felipe <geanfelipemoura@outlook.com>
2015-02-15 15:50:25 -0300

    debug=true

--- projeto/settings.py
+++ projeto/settings.py
@@ -23,7 +23,7 @@ import dj_database_url
 SECRET_KEY = 'v#l*!&ug770f7+5oz(2i92p3)+*#i1i!_yun50%5k21flxk&sk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = True
 
